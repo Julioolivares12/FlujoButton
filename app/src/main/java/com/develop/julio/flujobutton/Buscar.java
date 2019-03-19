90af89261ba6247916788ed5321fa132e7173062
julio <julioolivares90@gmail.com>
2019-03-19 16:28:39 -0600

    cambios

--- app/src/main/java/com/develop/julio/flujobutton/Buscar.java
+++ app/src/main/java/com/develop/julio/flujobutton/Buscar.java
@@ -1,6 +1,7 @@
 package com.develop.julio.flujobutton;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -8,6 +9,9 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class Buscar extends AppCompatActivity {
 
@@ -41,14 +45,45 @@ public class Buscar extends AppCompatActivity {
                         startActivity(irRegistro);
                         finish();
                     }else if (operacion.equals("eliminar")){
-                        Intent irEliminar = new  Intent(getApplicationContext(),Buscar.class);
+                        Intent irEliminar = new  Intent(getApplicationContext(),Datos.class);
                         irEliminar.putExtra("operacionP",operacion);
                         startActivity(irEliminar);
+                        finish();
                     }else {
-
+                        Intent irBuscar = new Intent(getApplicationContext(),Datos.class);
+                        irBuscar.putExtra("operacionP",operacion);
+                        Informacion usu = buscarUsuario(correo);
+                        irBuscar.putExtra("correoB",usu.getClass());
+                        startActivity(irBuscar);
+                        finish();
                     }
                 }
             }
         });
+    }
+
+
+    private Informacion buscarUsuario(final String e){
+        Informacion usuario = new Informacion();
+
+        /*
+         //forma con java 8 y los streams
+        Optional<Informacion> usuario  = Informacion.usuarios.stream()
+                .filter(u -> u.getCorreo()
+                        .equals(e))
+                .findFirst();*/
+        /*
+        *
+        * for (int i =0;i<Informacion.usuarios.size();i++){
+            if (Informacion.usuarios.contains(e)){
+                usuario = Informacion.usuarios.get(i);
+            }
+        }*/
+        //recorro todo el array de usuaios y si encuento un valor igual lo retorno
+        for (Informacion usu : Informacion.usuarios){
+            if (usu.getCorreo().equals(e))
+                usuario = usu;
+        }
+        return usuario;
     }
 }
